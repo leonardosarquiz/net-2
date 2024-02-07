0274b212f18fc5197e4ed2ba5760f8ad3a9845a7
Leonardo Sarquiz Hugenthobler <leonardosarquiz14@gmail.com>
2024-02-07 09:00:52 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,111 @@
+﻿using ControleDeContatos.Models;
+using ControleDeContatos.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ControleDeContatos.Controllers
+{
+    public class ContatoController : Controller
+    {
+        private readonly IContatoRepositorio _contatoRepositorio;
+        public ContatoController(IContatoRepositorio contatoRepositorio)
+        {
+            _contatoRepositorio = contatoRepositorio;
+        }
+        public IActionResult Index()
+        {
+            List<ContatoModel> contatos = _contatoRepositorio.BuscarTodos();
+
+            return View(contatos);
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar(int id)
+        {
+
+            ContatoModel contato = _contatoRepositorio.ListarPorId(id);
+           
+            return View(contato);
+        }
+
+
+        public IActionResult ApagarConfirmacao(int id)
+        {
+            ContatoModel contato = _contatoRepositorio.ListarPorId(id);
+            return View(contato);
+        }
+
+        public IActionResult Apagar(int id )
+        {
+            try
+            {
+                bool apagado = _contatoRepositorio.Apagar(id);
+                if (apagado)
+                {
+                    TempData["MensagemSucesso"] = "Contato  com sucesso";
+                }
+                else
+                {
+                    TempData["MensagemErro"] = "Ops, não conseguimos apagar o contato.";
+                }
+                return RedirectToAction("Index");
+
+
+
+            }
+            catch (System.Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos apagar o contato, mas detalhes do erro: {erro.Message}";
+                return RedirectToAction("Index");
+
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Criar(ContatoModel contato)
+        {
+           try
+            {
+                if (ModelState.IsValid)
+                {
+                  contato =  _contatoRepositorio.Adicionar(contato);
+                    TempData["MensagemSucesso"] = "Contato cadastrado com sucesso";
+                    return RedirectToAction("Index");
+                }
+
+                return View(contato);
+            } catch(System.Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos cadastrar seu contato, tente novamente, detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult Alterar(ContatoModel contato)
+        {
+          try
+            {
+                if (ModelState.IsValid)
+                {
+                    _contatoRepositorio.Atualizar(contato);
+                    TempData["MensagemSucesso"] = "Contato cadastrado com sucesso";
+                    return RedirectToAction("Index");
+                }
+                return View("Editar", contato);
+            }
+            catch(System.Exception erro)
+            {
+
+                TempData["MensagemErro"] = $"Ops, não conseguimos atualizar seu contato, tente novamente, detalhe do erro: {erro.Message}";
+                return RedirectToAction("Index");
+            }
+           
+        }
+    }
+}
---
+++ Repositorio/ContatoRepositorio.cs
@@ -0,0 +1,62 @@
+﻿using ControleDeContatos.Data;
+using ControleDeContatos.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ControleDeContatos.Repositorio
+{
+    public class ContatoRepositorio : IContatoRepositorio
+    {
+        private readonly BancoContext _bancoContext;
+        public ContatoRepositorio(BancoContext bancoContext)
+        {
+            _bancoContext = bancoContext;
+        }
+
+
+        public ContatoModel ListarPorId(int id)
+        {
+            return _bancoContext.Contatos.FirstOrDefault(x => x.Id == id);
+        }
+        public List<ContatoModel> BuscarTodos()
+        {
+            return _bancoContext.Contatos.ToList();
+        }
+        public ContatoModel Adicionar(ContatoModel contato)
+        {
+            _bancoContext.Contatos.Add(contato);
+            _bancoContext.SaveChanges();
+            return contato;
+        }
+
+        public ContatoModel Atualizar(ContatoModel contato)
+        {
+            ContatoModel conatatoDB = ListarPorId(contato.Id);
+
+            if (conatatoDB == null) throw new System.Exception("Houve um erro na atualização do contato");
+
+            conatatoDB.Nome = contato.Nome;
+            conatatoDB.Email = contato.Email;
+            conatatoDB.Celular = contato.Celular;
+
+
+            _bancoContext.Contatos.Update(conatatoDB);
+            _bancoContext.SaveChanges();
+
+            return conatatoDB;
+        }
+
+        public bool Apagar(int id)
+        {
+            ContatoModel conatatoDB = ListarPorId(id);
+
+            if (conatatoDB == null) throw new System.Exception("Houve um erro na deleção do contato");
+
+            _bancoContext.Contatos.Remove(conatatoDB);
+            _bancoContext.SaveChanges();
+
+            return true;
+
+        }
+    }
+}
